d4e086da2c2998200ca0f63e658d5455ebfae4f6
Tran <m_tran110573@ats-hr420-dm14.fc.ca>
2020-01-24 13:43:50 -0500

    done

--- js/test.js
+++ js/test.js
@@ -1,18 +1,21 @@
 (() => {
   // this is an IIFE -> also an implementation of the module pattern
 
-  // always go and get the elements you're working with
-  const mySpan = document.querySelector('main span');
+  // always go and get the elements you're working with - variable
+  const mySpan = document.querySelector('main span'),
+        myObject = document.querySelector('.svg-img');
 
   // functionality goes here => what we want to have happen
   function toggleSelection() {
     // we want to update our UI to make it look like we are making a selection
     //debugger;
     //add a CSS class to the element with JavaScript
-    this.classList.add("selected");
+    //toggle CSS class to the element with Javacript
+    this.classList.toggle("selected");
+    console.log(this.id)
   }
 
   // figure out what triggers will drive the functionality
   mySpan.addEventListener('click', toggleSelection);
-
+  myObject.addEventListener('click', toggleSelection);
 })()
